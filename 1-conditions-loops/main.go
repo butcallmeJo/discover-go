71917fb38bcade8bef350401376d2cd99426bd40
butcallmejo <13@holbertonschool.com>
2016-03-07 18:12:02 -0800

    removed some extra spaces that println puts automatically

--- 1-conditions-loops/main.go
+++ 1-conditions-loops/main.go
@@ -16,13 +16,13 @@ func main() {
 	}
 	school := "Holberton School"
 	beautifulWeather := true
-	holbertonFounders := []string{"Rudy Rigot", "Julien Barbier", "Sylvain Kalache"}
-	fmt.Println("my random number is ", num, " and is ", sign, " than 50")
-	fmt.Println("I am a student of the ", school)
+	holbertonFounders := []string{"Rudy Rigot", "Sylvain Kalache", "Julien Barbier"}
+	fmt.Println("my random number is", num, "and is", sign, "than 50")
+	fmt.Println("I am a student of the", school)
 	if beautifulWeather == true {
 		fmt.Println("It's a beautiful weather!")
 	}
 	for i := 0; i < 3; i++ {
-		fmt.Println(holbertonFounders[i], " is a founder")
+		fmt.Println(holbertonFounders[i], "is a founder")
 	}
 }
