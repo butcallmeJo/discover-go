2992d08eab36cc8b15fab4b38fa53626a0384331
butcallmejo <13@holbertonschool.com>
2016-03-07 18:09:02 -0800

    added error checking

--- 2-struct/main.go
+++ 2-struct/main.go
@@ -14,11 +14,14 @@ type user struct {
 func main() {
 	u := user{Name: "Betty Holberton", DOB: "March 7, 1917", City: "Philadelphia"}
 	layout := "January 2, 2006"
-	then, _ := time.Parse(layout, u.DOB)
+	then, err := time.Parse(layout, u.DOB)
+	if err != nil {
+		fmt.Printf("error parsing the DOB layout")
+	}
 	ageDuration := time.Since(then)
 	ageHours := ageDuration.Hours()
 	age := int(ageHours / 8760)
 	fmt.Println("Hello", u.Name)
-	fmt.Println(u.Name, " who was born in ", u.City, " would be ", age, " years old today.")
+	fmt.Println(u.Name, "who was born in", u.City, "would be", age, "years old today.")
 
 }
